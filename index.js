7f9b67dd172c8c6303f3265325d42dc712f0692f
teramotodaiki <i101323@gmail.com>
2019-04-19 00:37:40 +0900

    chore: split

--- index.js
+++ index.js
@@ -33,38 +33,6 @@ const zip = new JSZip();
     });
 })();
 
-function processPage(data, json) {
-  const page = JSON.parse(json);
-  const title = findObject(page, "text", "title");
-  if (title) {
-    title.attributedString.string = data.title + "　"; // 英語だとフォントが変わる？ので、全角スペースを入れる
-  }
-  const author = findObject(page, "text", "author");
-  if (author) {
-    author.attributedString.string = data.author + "　"; // 英語だとフォントが変わる？ので、全角スペースを入れる
-  }
-  const thumbnail = findObject(page, "bitmap", "gameThum");
-  if (thumbnail) {
-    thumbnail.image._ref = data.thumbnail;
-  }
-  const qr = findObject(page, "bitmap", "qr");
-  if (qr) {
-    qr.image._ref = data.qr;
-  }
-  return JSON.stringify(page);
-}
-
-function findObject(object, _class, name) {
-  if (object._class === _class && object.name === name) return object;
-  if (Array.isArray(object.layers)) {
-    for (const child of object.layers) {
-      const result = findObject(child, _class, name);
-      if (result !== null) return result;
-    }
-  }
-  return null;
-}
-
 function addRecursive(data, rootPath, relativePath = "") {
   const absolutePath = path.join(rootPath, relativePath);
   const stat = fs.statSync(absolutePath);
@@ -79,7 +47,8 @@ function addRecursive(data, rootPath, relativePath = "") {
   } else if (stat.isFile()) {
     if (/pages\/.+\.json$/i.test(absolutePath)) {
       const text = fs.readFileSync(absolutePath, "utf8");
-      zip.file(relativePath, processPage(data, text));
+      const convert = require("./convert");
+      zip.file(relativePath, convert(data, text));
     } else {
       const buffer = fs.readFileSync(absolutePath);
       zip.file(relativePath, buffer);
